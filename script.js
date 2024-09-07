0ac6dcbcf789c0f5b338588630c2e623975ce5ca
Klaska <laska.rayl@gmail.com>
2024-09-08 00:03:03 +0200

    finally made the outcome function functional

--- script.js
+++ script.js
@@ -3,12 +3,13 @@ const degrees = document.querySelector('input');
 const para = document.querySelector('p');
 let body = document.querySelector('body');
 const btn = document.querySelector('button');
-
+let choice;
+let degreeValue;
 
 selector.addEventListener('change', howMany);
 
 function howMany(event) {
-    let choice = selector.value;
+    choice = selector.value;
 
     if (typeof choice === 'string') {
     selector.style.display = 'none';
@@ -24,6 +25,18 @@ btn.addEventListener('click', getDegrees);
 function getDegrees() {
     degrees.style.display = 'none';
     btn.style.display = 'none';
-    let degreeValue = degrees.value;
+    degreeValue = degrees.value;
     console.log(degreeValue);
+    outcome();
+}
+
+function outcome() {
+    if (choice === 'sun' && degreeValue >= 25) {
+        para.textContent = 'it is nice and sunny';
+        console.log('hothot');
+    } else if (choice === 'sun' && degreeValue <= 25) {
+        para.textContent = 'it is sunny but not that hot';
+    } else if (choice === 'sun' && degreeValue >= 50) {
+        para.textContent = 'it is boiling hot';
+    }
 }
