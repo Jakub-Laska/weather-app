9d82230d53b95875b6a4feefe22bb35e391bb452
Klaska <laska.rayl@gmail.com>
2024-09-08 18:42:31 +0200

    outcome function is done

--- script.js
+++ script.js
@@ -7,20 +7,21 @@ const backBtn = document.querySelector('#goBack');
 let choice;
 let degreeValue;
 
-selector.addEventListener('change', howMany);
+selector.addEventListener('change', showInput);
 
-function howMany(event) {
+function showInput() {
     choice = selector.value;
 
     if (typeof choice === 'string') {
     selector.style.display = 'none';
     degrees.style.display = 'inline-block';
     btn.style.display = 'inline-block';
+    backBtn.style.display = 'inline-block';
 } else {
     console.log('whoops');
 }
 }
-
+// Function responsible for submition, getting the value of the input and launching main function
 btn.addEventListener('click', getDegrees);
 
 function getDegrees() {
@@ -31,18 +32,103 @@ function getDegrees() {
     console.log(degreeValue);
     outcome();
 }
-
+// Main function responsible for the outcome
 function outcome() {
-    if (choice === 'sun' && degreeValue >= 25) {
-        para.textContent = 'it is nice and sunny';
-        console.log('hothot');
-    } else if (choice === 'sun' && degreeValue <= 25) {
-        para.textContent = 'it is sunny but not that hot';
-    } else if (choice === 'sun' && degreeValue >= 50) {
-        para.textContent = 'it is boiling hot';
+    // if it is sunny
+    if (choice === 'sun') {
+        if (degreeValue >= 45) {
+            para.textContent =
+            `extremely hot weather it is ${degreeValue} degrees, lots of sun today`;
+        } else if (degreeValue >= 25 && degreeValue < 45) {
+            para.textContent =
+            `It is ${degreeValue} degrees, nice and sunny today`;
+        } else if (degreeValue < 25 && degreeValue >= 5) {
+            para.textContent =
+            `it is ${degreeValue} degrees, sunny but it could be a little cold today`;
+        } else if (degreeValue < 5 && degreeValue >= -10){
+            para.textContent =
+            `is is ${degreeValue} degrees, sunny but it is really cold`;
+            } else {
+                para.textContent =
+                `extremely cold weather ${degreeValue} degrees, but at least it is sunny today`;
+            }
+    // if it is cloudy
+    } else if (choice === 'cloud') {
+        if (degreeValue >= 45) {
+            para.textContent =
+            `extremely hot weather it is ${degreeValue} degrees, lots of clouds today`;
+        } else if (degreeValue >= 25 && degreeValue < 45) {
+            para.textContent =
+            `It is ${degreeValue} degrees, hot but cloudy today`;
+        } else if (degreeValue < 25 && degreeValue >= 5) {
+            para.textContent =
+            `it is ${degreeValue} degrees, cloudy and it could be a little cold today`;
+        } else if (degreeValue < 5 && degreeValue >= -10){
+            para.textContent =
+            `is is ${degreeValue} degrees, cloudy and really cold`;
+            } else {
+                para.textContent =
+                `extremely cold weather ${degreeValue} degrees, with lots of clouds`;
+            }
+    // if it is rainy
+    } else if (choice === 'rain') {
+        if (degreeValue >= 45) {
+            para.textContent =
+            `extremely hot weather it is ${degreeValue} degrees, lots of rain in some parts of the today`;
+        } else if (degreeValue >= 25 && degreeValue < 45) {
+            para.textContent =
+            `It is ${degreeValue} degrees, hot but rainy today`;
+        } else if (degreeValue < 25 && degreeValue >= 5) {
+            para.textContent =
+            `it is ${degreeValue} degrees, rainy and it could be a little cold today`;
+        } else if (degreeValue < 5 && degreeValue >= -10){
+            para.textContent =
+            `is is ${degreeValue} degrees, rainy with a high probability of sleet and it is really cold today`;
+            } else {
+                para.textContent =
+                `extremely cold weather ${degreeValue} degrees, with lots of hail or sleet`;
+            }
+    // if it is windy
+    } else if (choice === 'wind') {
+        if (degreeValue >= 45) {
+            para.textContent =
+            `extremely hot weather it is ${degreeValue} degrees, lots of wind of the today`;
+        } else if (degreeValue >= 25 && degreeValue < 45) {
+            para.textContent =
+            `It is ${degreeValue} degrees, hot but windy today`;
+        } else if (degreeValue < 25 && degreeValue >= 5) {
+            para.textContent =
+            `it is ${degreeValue} degrees, windy and it could be a little cold today`;
+        } else if (degreeValue < 5 && degreeValue >= -10){
+            para.textContent =
+            `is is ${degreeValue} degrees, windy and it is really cold today`;
+            } else {
+                para.textContent =
+                `extremely cold weather ${degreeValue} degrees, with lots of wind`;
+            }
+    // if it is snowy
+    } else {
+        if (degreeValue >= 45) {
+            para.textContent =
+            `extremely hot weather it is ${degreeValue} degrees, no chance of snow today`;
+        } else if (degreeValue >= 25 && degreeValue < 45) {
+            para.textContent =
+            `It is ${degreeValue} degrees, hot and no chance of snow today`;
+        } else if (degreeValue < 25 && degreeValue >= 5) {
+            para.textContent =
+            `it is ${degreeValue} degrees, it could snow today`;
+        } else if (degreeValue < 5 && degreeValue >= -10){
+            para.textContent =
+            `is is ${degreeValue} degrees, snowy and it is really cold today`;
+            } else {
+                para.textContent =
+                `extremely cold weather ${degreeValue} degrees, with lots of snowing`;
+            }
     }
 }
 
+
+// refresh button
 backBtn.addEventListener('click', refresh);
 
 function refresh() {
